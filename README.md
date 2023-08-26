d88e35b04758afc98bd929a63a7ac9a2432388f6
Bengemon825 <bgree825@gmail.com>
2023-08-26 16:56:43 -0400

    refactor structure, move background and logo into Home, call Video in Home instead of index.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Website or something idk
+# The Judgies Pod Website
+
+## Todo:
+- fix logo at the top of the page
+- add a "find us" section with links to the socials
+- add a carrot to go with the faces (balances out so 2 and 2 gifs)
---
+++ judgies-website/src/components/Home/HomeIndex.js
@@ -0,0 +1,24 @@
+import logo from "../../media/channelPfp.png";
+import background from "../../media/90sBackground.jpg";
+import "./Home.css"
+import Video from "../Video/VideoIndex"
+
+function Home() {
+  return (
+    <div
+      className="Home"
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
+
+      <div className="LogoContainer">
+        <img className="Logo" src={logo} alt="Logo" />
+      </div>
+      <Video/>
+      
+    </div>
+  );
+}
+
+export default Home;
